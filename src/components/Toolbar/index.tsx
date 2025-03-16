b24ce070d58d0280acd2b12e52c8261bfef8d356
legochiang <qas612820704@gmail.com>
2025-03-16 13:41:59 +0800

    add: title marque

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Avatar } from '@mui/material';
-import { AppContext } from '../../contexts';
+import { AppBar, Toolbar, Typography, Tooltip, Paper, Box } from '@mui/material';
 import UserButton from './UserButton';
+import './index.css';
 
 export default function BoardToolbar() {
     return (
-        <AppBar position="static" color="transparent" elevation={0} sx={{ padding: 0 }}>
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between", padding: 0 }}>
-                <Typography variant="h6" color="primary">
-                    主要畫面
-                </Typography>
+        <AppBar position="static" color="transparent" elevation={0} sx={{ padding: '0 !important' }}>
+            <Toolbar sx={{ display: "flex", justifyContent: "space-between", padding: '0 !important' }}>
+                <Tooltip title="你好呀～要不要跟大家分享你可愛的貓貓卡片？登入後，就可以上傳你的照片了呦！">
+                    <Paper sx={{ border: '1px solid #E0E0E0', backgroundColor: 'white', padding: '4px 8px', overflow: 'hidden' }}>
+                        <Box sx={{ display: 'flex', whiteSpace: 'nowrap', animation: 'marquee 8s linear infinite' }}>
+                            <Typography variant="body2">
+                                你好呀～要不要跟大家分享你可愛的貓貓卡片？登入後，就可以上傳你的照片了呦！
+                            </Typography>
+                        </Box>
+                    </Paper>
+                </Tooltip>
                 <UserButton />
             </Toolbar>
         </AppBar>
