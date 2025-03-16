2e00baf15f80132e7624817d6ac18b69b122219a
legochiang <qas612820704@gmail.com>
2025-03-16 12:02:54 +0800

    chore: adjust client_id

--- src/supabase/index.js
+++ src/supabase/index.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ozpvjrrnpsvfbzfndgub.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cHZqcnJucHN2ZmJ6Zm5kZ3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMzA5ODIsImV4cCI6MjA1NzYwNjk4Mn0.96mmLiJ-9GDd8_gnSFkJ02nG2w4p4BDj471kxO-dFZE";
+const supabaseUrl = "https://nuknwpmknuvxtrcujnfj.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51a253cG1rbnV2eHRyY3VqbmZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwOTQ4MjksImV4cCI6MjA1NzY3MDgyOX0.l-yWej8wR815Si5QL217ootIsYQhqF2M7xTN0MjeD6Q";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
